92a037101a52fb199ea5aeb0c1c5a3fdf5a71294
Hannah Voelker <hannahvoelker13@gmail.com>
2016-06-07 09:40:40 -0400

    Add files via upload

---
+++ makepiechart.R
@@ -0,0 +1,60 @@
+
+#!/usr/bin/env Rscript
+#
+# makepiechart.r
+# Author: Hannah Voelker
+#Last modified: 5/5/16
+#Adapted from printPieChart.r
+# Given a set of the 10 most prevalent bacteria in a sample, creates a corresponding pie chart 
+#
+
+#import
+require(plotrix)
+library(extrafont)
+
+# get the csv file
+genuscounts <<- read.csv(file.choose(), header = TRUE, sep = ",")
+
+pdf(file = "SizerSchool_Piecharts.pdf", width = 8.5, height = 11, onefile = TRUE, family = "Arial Unicode MS")
+
+#loop through the data frame and find the top ten for each 
+for(i in 2:ncol(genuscounts)) {
+    thisColumn <- genuscounts[,c(1,i)]
+    samplename <- names(genuscounts[i])
+    topten <- head(thisColumn[order(thisColumn[,2], decreasing = TRUE), ], 10)
+    #We have to create an "other" factor using the remaining data
+    # I did this by making a new data frame
+    other <- sum(genuscounts[,i])-sum(topten[,2])
+    otherdata <- data.frame("Genus" = character(1), samplename = numeric(1), stringsAsFactors = FALSE)
+    names(otherdata)[2] = samplename
+    otherdata[1,] <- c("Other", other)
+    #now that the names align, we can attach the two frames together
+    topten <- rbind(topten, otherdata)
+    
+    #now to make the proportions
+    totalcount <- sum(genuscounts[,i])
+    slices = as.integer(topten[,2])/totalcount
+    
+  # add labels
+  slices = round(slices*100,2)
+  lbls <- topten[,1]
+  lbls = paste(lbls,as.character(slices))
+  lbls = paste(lbls,"%",sep="")
+
+
+  colors = rainbow(length(slices), s = 0.5)
+  pie(slices, '', radius = 0.63, col=colors)
+  # can use font.main = '' to adjust the font of main title
+  textX = 0
+  textY = 1.2
+  text(textX,textY, samplename, cex=6, font= 6, family = "Arial Rounded MT Bold")
+  legend("bottomleft", lbls, cex=1.1, fill=colors)
+  textX = 0
+  textY = 0.85
+  text(textX,textY,'This sample\' s microbiome, \n broken down by genus:', cex=2.5, font=4, family="Arial Unicode MS")
+  textX = 0.5
+  textY = -1.0
+  text(textX,textY,' DISCLAIMER:\n Results cannot be\n used to draw any\n conclusions about\n health information.', cex=1, adj=c(0,NA), font=0.75, col = "gray46", family="Georgia")
+} 
+dev.off()
+# this is the end of the for loop
---
+++ makevenndiag.R
@@ -0,0 +1,92 @@
+#!/usr/bin/env Rscript
+#
+# makevenndiag.r
+# Modified by Hannah Voelker
+# Copyright (c) 2016 BioSeq
+#
+# makes a pdf of venn diagrams for a set of samples 
+#
+#import
+require(plotrix)
+library(extrafont)
+
+genuscounts <<- read.csv(file.choose(), header = TRUE, sep = ",")
+genuspairs <<- read.table(file.choose(), header= FALSE)
+
+pdf(file= "Sizer_venndiagrams.pdf", height = 11, width = 8.5, onefile = TRUE)
+#loop through the .txt file of pairs and make a Venn diagram for each
+for (j in 1:nrow(genuspairs)){
+sample1 <- genuspairs[j,1]
+sample2 <- genuspairs[j,2]
+
+#now loop through all the columns to find these two samples
+# set up the top ten names
+for(i in 2:ncol(genuscounts)){
+  if (sample1 == names(genuscounts[i])){
+    thisColumn <- genuscounts[,c(1,i)]
+    topten1 <- head(thisColumn[order(thisColumn[,2],decreasing = TRUE), ], n = 10)
+  }
+  if(sample2 == names(genuscounts[i])){
+    thisColumn <- genuscounts[,c(1,i)]
+    topten2 <- head(thisColumn[order(thisColumn[,2], decreasing = TRUE), ], n = 10)
+  }
+}
+# we have to make these samples ready for the Venn diagram
+makeVennList <- function(vennList){
+  vennList <- unlist(strsplit(as.character(vennList), ","))
+  indexofOther = match("Other", vennList)
+  return(vennList)
+}
+vennList1 <- makeVennList(topten1[,1])
+vennList2 <- makeVennList(topten2[,1])
+
+par(mfrow=c(3,1),mar=c(0,0,0,0))
+
+#parameters for layout of the Venn diagram and text
+#vennX1 = 0.375 centers the Venn diagram on the page
+radius = 0.2
+vennX1=0.625; vennY1=0.5
+vennX2=vennX1+radius; vennY2=vennY1
+#placement of lists of microbes within each circle
+textX1=vennX1-0.05; textY1=vennY1+0.3
+textX2=vennX1+0.18; textY2=textY1
+textSize = 1.2
+#placement of the title near the top of each circle (e.g. SH07_RC)
+titleX1=textX1+0.02; titleY1=textY1+0.05
+titleX2=titleX1+0.26; titleY2=titleY1
+titleSize = 1.95
+#first Venn diagram has message 1, second and third Venn diagrams have message 2
+message1 = "SAME person, DIFFERENT body sites"
+message2 = "DIFFERENT people, SAME body site"
+
+
+
+drawVennDiagram <- function(message, leftSampleName, rightSampleName, leftVennList, rightVennList){
+  draw.circle(vennX1,vennY1,radius,nv=100,border=NULL,col=NA,lty=1,lwd=1)
+  draw.circle(vennX2,vennY1,radius,nv=100,border=NULL,col=NA,lty=1,lwd=1)
+  legend(textX1,textY1, leftVennList, cex=textSize, bty="n",adj=c(1,NA))
+  legend(textX2,textY2, rightVennList, cex=textSize, bty="n")
+  text(titleX1,titleY1,leftSampleName, cex=titleSize, font=2, family = "Arial Rounded MT Bold")
+  text(titleX2,titleY2,rightSampleName, cex=titleSize, font=2, family = "Arial Rounded MT Bold")
+  text(titleX1+0.02,titleY1+0.08,"Sample 1", cex=0.8, font=3)
+  text(titleX2-0.02,titleY2+0.08,"Sample 2", cex=0.8, font=3)
+  text(0,0.9, message, cex=2, font=2, adj=c(0,NA), family = "Arial Rounded MT Bold")
+  text(0,0.5, "How many microbes are found in BOTH Sample 1 and Sample 2?
+       Answer 1:\n\n\n\n
+       How many different microbes are found in either sample?
+       (Don\' t count any more than once.)
+       Answer 2:\n\n\n\n
+       Calculate the Jaccard Similarity:
+       Divide Answer 1 by Answer 2 and multiply by 100%",
+       cex=1, font=2, adj=c(0,NA), family = "Arial Unicode MS")
+}
+
+#Draw all three Venn diagrams
+plot.new()
+drawVennDiagram(message1, sample1, sample2, vennList1, vennList2)
+plot.new()
+drawVennDiagram(message2, sample1, "", vennList1, "")
+plot.new()
+drawVennDiagram(message2, sample2, "", vennList2, "")
+}
+dev.off()
