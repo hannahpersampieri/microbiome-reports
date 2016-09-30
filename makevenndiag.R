5df04e68925543084f76574ddef473c6ea132f0d
Hannah <hannahvoelker13@gmail.com>
2016-09-30 09:26:13 -0400

    Update makevenndiag.R

--- makevenndiag.R
+++ makevenndiag.R
@@ -40,21 +40,21 @@ makeVennList <- function(vennList){
 vennList1 <- makeVennList(topten1[,1])
 vennList2 <- makeVennList(topten2[,1])
 
-par(mfrow=c(3,1),mar=c(0,0,0,0))
+par(mfrow=c(2,1))
 
 #parameters for layout of the Venn diagram and text
 #vennX1 = 0.375 centers the Venn diagram on the page
-radius = 0.2
-vennX1=0.625; vennY1=0.5
+radius = 0.24
+vennX1=0.375; vennY1=0.45
 vennX2=vennX1+radius; vennY2=vennY1
 #placement of lists of microbes within each circle
 textX1=vennX1-0.05; textY1=vennY1+0.3
-textX2=vennX1+0.18; textY2=textY1
-textSize = 1.2
+textX2=vennX1+0.22; textY2=textY1
+textSize = 0.88
 #placement of the title near the top of each circle (e.g. SH07_RC)
-titleX1=textX1+0.02; titleY1=textY1+0.05
-titleX2=titleX1+0.26; titleY2=titleY1
-titleSize = 1.95
+titleX1=textX1+0.03; titleY1=textY1+0.06
+titleX2=titleX1+0.28; titleY2=titleY1
+titleSize = 1.80
 #first Venn diagram has message 1, second and third Venn diagrams have message 2
 message1 = "SAME person, DIFFERENT body sites"
 message2 = "DIFFERENT people, SAME body site"
@@ -70,15 +70,7 @@ drawVennDiagram <- function(message, leftSampleName, rightSampleName, leftVennLi
   text(titleX2,titleY2,rightSampleName, cex=titleSize, font=2, family = "Arial Rounded MT Bold")
   text(titleX1+0.02,titleY1+0.08,"Sample 1", cex=0.8, font=3)
   text(titleX2-0.02,titleY2+0.08,"Sample 2", cex=0.8, font=3)
-  text(0,0.9, message, cex=2, font=2, adj=c(0,NA), family = "Arial Rounded MT Bold")
-  text(0,0.5, "How many microbes are found in BOTH Sample 1 and Sample 2?
-       Answer 1:\n\n\n\n
-       How many different microbes are found in either sample?
-       (Don\' t count any more than once.)
-       Answer 2:\n\n\n\n
-       Calculate the Jaccard Similarity:
-       Divide Answer 1 by Answer 2 and multiply by 100%",
-       cex=1, font=2, adj=c(0,NA), family = "Arial Unicode MS")
+  text(0,0.99, message, cex=2, font=2, adj=c(0,NA), family = "Arial Rounded MT Bold")
 }
 
 #Draw all three Venn diagrams
@@ -88,5 +80,15 @@ plot.new()
 drawVennDiagram(message2, sample1, "", vennList1, "")
 plot.new()
 drawVennDiagram(message2, sample2, "", vennList2, "")
+plot.new()
+text(0,0.5, " Calculate for every pair of samples: \n 
+  How many microbes are found in BOTH Sample 1 and Sample 2?
+  Answer 1: \n\n\n\n
+  How many microbes are found in either sample?
+  Answer 2: \n\n\n\n
+  Calculate the Jaccard Similarity:
+  Divide Answer 1 by Answer 2 and multiple by 100%", 
+  cex = 1, font = 1.75, adj=c(0,NA), family = "Arial Unicode MS"
+)
 }
 dev.off()
